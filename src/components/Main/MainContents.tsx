82b9c7f2cfb1089d72745809832d447ca4a0f6b9
shift <qjfrntop12@naver.com>
2019-09-19 22:46:39 +0900

    Add main route

--- src/components/Main/MainContents.tsx
+++ src/components/Main/MainContents.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+
 import './MainContents.scss';
 
 const MainContents = () => (
-  <div>안녕</div>
+  <div className="MainContent">
+    <div className="title">
+      전국 프로듀서 노래자랑
+      <div className="subtitle">
+        시즌 8 시상식 시청자 투표
+      </div>
+    </div>
+    <hr className="contour" />
+    <Link to="/overview" className="next">시작하기 →</Link>
+  </div>
 );
 
 export default MainContents;
