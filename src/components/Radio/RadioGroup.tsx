ec74f28d4f1cf6de6c4f020ff21ecb741c5883cf
shift <qjfrntop12@naver.com>
2019-09-26 00:18:57 +0900

    Add radio button component

--- src/components/Radio/RadioGroup.tsx
+++ src/components/Radio/RadioGroup.tsx
@@ -2,11 +2,17 @@ import React, { useState } from 'react';
 
 import RadioButton from './RadioButton';
 
+type Prop = { values: string[], onChanged: (index: number) => void };
+
 const RadioGroup = (props: any) => {
-  const { values }: { values: string[] } = props;
+  const { values, onChanged }: Prop = props;
   const [selected, setSelect] = useState(-1);
   const onClickEvent = (index: number) => {
+    if (selected === index) {
+      return;
+    }
     setSelect(index);
+    onChanged(index);
   };
   const selectionComps = values.map((value: string, index) => (
     <RadioButton selected={selected} value={value} index={index} onClick={onClickEvent} />
