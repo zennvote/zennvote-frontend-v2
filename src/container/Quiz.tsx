43e50ef2a40c156dc736f51cbb66e1546547f7b2
shift <qjfrntop12@naver.com>
2019-09-25 23:32:04 +0900

    Add quiz page

---
+++ src/container/Quiz.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import QuizContents from '../components/Quiz/QuizContents';
+
+const Main = () => (
+  <div className="Quiz">
+    <QuizContents />
+  </div>
+);
+
+export default Main;
