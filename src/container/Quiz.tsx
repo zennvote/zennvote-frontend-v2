12c01b764c23d16903adbc5f4a40187b6d1dfcef
shift <qjfrntop12@naver.com>
2019-09-27 18:44:17 +0900

    Separate quiz router into overview and contents

--- src/container/Quiz.tsx
+++ src/container/Quiz.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
-import QuizContents from '../components/Quiz/QuizContents';
+import queryParser from 'query-string';
 
-const Main = () => (
+import QuizContent from '../components/Quiz/QuizContents';
+import QuizOverview from '../components/Quiz/QuizOverview';
+import RadioGroup from '../components/Radio/RadioGroup';
+
+const getOverview = () => (
   <div className="Quiz">
-    <QuizContents />
+    <QuizOverview />
   </div>
 );
+
+const getContent = (index: number) => (
+  <div className="Quiz">
+    <QuizContent index={index} />
+  </div>
+);
+
+const Main = ({ location, match }: any) => {
+  const query = queryParser.parse(location.search);
+  const { index } = query;
+  if (index === undefined) {
+    return getOverview();
+  }
+  return getContent(Number(index));
+};
 
 export default Main;
